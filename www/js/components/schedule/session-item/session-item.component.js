1edf9e0348647422a799fad0cc002d6aaa953684
Conner Dodge <connerdodge@u.boisestate.edu>
2017-09-28 08:30:44 -0600

    Cleaned up session list and session page to not cut off title

--- www/js/components/schedule/session-item/session-item.component.js
+++ www/js/components/schedule/session-item/session-item.component.js
@@ -4,8 +4,7 @@
     .component('sessionListItem', {
       template: '' +
       '<ion-item ui-sref="app.session({ sessionID: $ctrl.session.id })">' +
-        '<div ng-bind="$ctrl.session.title"></div>' +
-        '<div ng-bind="$ctrl.session.subtitle"></div>' +
+        '<div ng-bind="$ctrl.session.title" class="session-list-title"></div>' +
       '</ion-item>',
       bindings: {
         session: '<'
