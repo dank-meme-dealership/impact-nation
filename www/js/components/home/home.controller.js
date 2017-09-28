f78a8c59e2af85275ab7a80d57d8fc292a0b7976
Conner Dodge <connerdodge@u.boisestate.edu>
2017-09-28 12:37:39 -0600

    Added next sessions list to home page

--- www/js/components/home/home.controller.js
+++ www/js/components/home/home.controller.js
@@ -3,7 +3,19 @@
     .module('impactNation.home')
     .controller('HomeController', HomeController);
 
-  function HomeController() {
+  function HomeController($rootScope, scheduleManager, sessionsService) {
     var $ctrl = this;
+    updateUserSessions();
+
+    $rootScope.$on('scheduleManager.toggleScheduled', updateUserSessions);
+
+    function updateUserSessions() {
+      var mySchedule = scheduleManager.getMySchedule();
+      sessionsService.getSessions().then(function (sessions) {
+        $ctrl.userSessions = _.filter(sessions, function (session) {
+          return _.includes(mySchedule, parseInt(session.id));
+        }).slice(0,3);
+      })
+    }
   }
 })();
