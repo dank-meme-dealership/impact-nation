3650be9aa6d766cf99d1b3237f4b44656d53a522
Matttaylor8910 <taylor.matt777@gmail.com>
2017-09-29 01:36:16 +0700

    Add sessions to speaker pages

--- www/js/components/speakers/speaker/speaker.controller.js
+++ www/js/components/speakers/speaker/speaker.controller.js
@@ -3,7 +3,7 @@
     .module('impactNation.speakers.speaker')
     .controller('SpeakerController', SpeakerController);
 
-  function SpeakerController($state, speakersService) {
+  function SpeakerController($state, speakersService, sessionsService) {
     var $ctrl = this;
 
     init();
@@ -11,7 +11,19 @@
     function init() {
       speakersService.getSpeaker($state.params.speakerID).then(function (speaker) {
         $ctrl.speaker = speaker;
-      })
+
+        sessionsService.getSessions().then(function (sessions) {
+          $ctrl.sessions = _.filter(sessions, function (session) {
+            var hasSpeaker = false;
+            _.each(session.speakers, function (speaker) {
+              if (speaker.id === $ctrl.speaker.id) {
+                hasSpeaker = true;
+              }
+            });
+            return hasSpeaker;
+          });
+        });
+      });
     }
   }
 })();
