7ee9c1e6f02ceb727f6de86fab5c664567ee7d41
Conner Dodge <connerdodge@u.boisestate.edu>
2017-09-28 13:46:32 -0600

    Changed to ng-src

--- www/js/components/speakers/speaker-item/speaker-item.component.js
+++ www/js/components/speakers/speaker-item/speaker-item.component.js
@@ -4,7 +4,7 @@
     .component('speakerListItem', {
       template: '' +
         '<ion-item ui-sref="app.speaker({ speakerID: $ctrl.speaker.id })">' +
-          '<img class="speaker-avatar" src="{{ $ctrl.speaker.imageUrl }}">' +
+          '<img class="speaker-avatar" ng-src="{{ $ctrl.speaker.imageUrl }}">' +
           '<div class="speaker-info"><h2 class="speaker-name" ng-bind="$ctrl.speaker.name"></h2>' +
           '<h4 class="speaker-title-item" ng-bind="$ctrl.speaker.title"></h4></div>' +
         '</ion-item>',
